930e4cceaaa530783cfe6694f05d420df51e1289
sebbbin <nsb08045@daum.net>
2024-05-15 11:01:50 +0900

    #1 - [Remove] 프로젝트 파일 삭제

--- practice/src/main/java/org/sopt/practice/exception/CustomizedException.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.practice.exception;
-
-import org.sopt.practice.common.dto.ErrorMessage;
-
-public class CustomizedException extends BusinessException {
-    public CustomizedException(ErrorMessage errorMessage) {
-        super(errorMessage);
-    }
-}
