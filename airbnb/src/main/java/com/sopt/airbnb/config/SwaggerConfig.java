9c17f770f6d2feb19f0d2a50b372c41dc1288386
ckkim817 <ckkim817@konkuk.ac.kr>
2024-05-16 21:59:30 +0900

    #8 [Feat] Swagger 세팅

---
+++ airbnb/src/main/java/com/sopt/airbnb/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.sopt.airbnb.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("airbnb 게 섯거라")
+                .description("NOW SOPT 합동 세미나 웹 2팀 공주님들이 사용하실 API입니다.")
+                .version("v1");
+
+        return new OpenAPI()
+                .info(info);
+    }
+}
--- airbnb/src/main/java/com/sopt/airbnb/controller/RoomController.java
+++ airbnb/src/main/java/com/sopt/airbnb/controller/RoomController.java
@@ -2,12 +2,19 @@ package com.sopt.airbnb.controller;
 
 import com.sopt.airbnb.service.RoomService;
 import com.sopt.airbnb.service.dto.RoomListDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "숙소 관련 API")
 @RestController
 @RequestMapping("/api/v1/rooms")
 @RequiredArgsConstructor
@@ -15,6 +22,22 @@ public class RoomController {
 
     private final RoomService roomService;
 
+    @Operation(
+            summary = "숙소 리스트 조회 API",
+            description = "전체 숙소 목록을 조회하는 API입니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "숙소 리스트 조회 성공",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = RoomListDto.class
+                                    )
+                            )
+                    )
+            }
+    )
     @GetMapping
     public ResponseEntity<RoomListDto> getRoomList() {
         return ResponseEntity.ok(roomService.findRoomList());
