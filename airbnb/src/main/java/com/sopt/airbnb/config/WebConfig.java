44696f7d6134679e272c97c24ba130118f6cb700
ckkim817 <ckkim817@konkuk.ac.kr>
2024-05-22 20:20:59 +0900

    [Hotfix] 스웨거 오류 해결을 위한 CORS allowedOrigins 추가

--- airbnb/src/main/java/com/sopt/airbnb/config/WebConfig.java
+++ airbnb/src/main/java/com/sopt/airbnb/config/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:5173",
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://localhost:5173",
                         "https://client-omega-blush.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
-    
+
 }
